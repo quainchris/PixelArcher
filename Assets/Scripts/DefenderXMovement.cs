7bf72602fd11996edab36bcad7c19ae16dc33278
sheeno27 <danielsheehan@rocketmail.com>
2015-06-01 14:39:24 +0100

    Boss - DefenderX
    Added a boss spritesheet to the game. The boss has 3 blue orbs attached
    to it. When 3 arrows hit any of the orbs on the wings then play the
    corresponding animation which displays the ship without that wing. Only
    when the orb at the very top gets hit 3 times will the ship be
    destroyed.
    I have all the animations to make this happen apart from the ship
    exploding maybe. The ship should shoot a rocket at the ground when a ray
    cast coming from the turret detects the player as it passes by. You
    could increase the frequency of the rockets when the ship loses a wing,
    could be fun. The could have basic AI which follows the player instead
    of having set movement.

---
+++ Assets/Scripts/DefenderXMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderXMovement : MonoBehaviour {
+	private Vector3 hover = Vector3.left * 0.1f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.x > 7)
+			hover = Vector3.left * 0.1f;
+			
+		else if (this.transform.position.x < -7)
+			hover = Vector3.right * 0.1f;
+		
+		transform.Translate(hover);
+	}
+	
+	
+}
